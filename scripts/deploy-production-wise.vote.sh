fd97de2af9ed19dbe70ab406d812c90e090cdda9
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-01-04 19:14:29 +0100

    ci: enable ansible pipelining

--- scripts/deploy-production-wise.vote.sh
+++ scripts/deploy-production-wise.vote.sh
@@ -3,8 +3,8 @@ set -e # fail on first error
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.." # parent dir
 cd "${DIR}"
 
-ansible-playbook -i wise.vote/production.yml \
+ANSIBLE_CONFIG="${DIR}/../ansible.cfg" ansible-playbook -i wise.vote/production.yml \
     --extra-vars "@~/p/ansible/mort.yml" \
     --vault-id jblew_production@prompt \
-    --vault-id generic@prompt -vv wise.vote/wise.vote.yml \
+    --vault-id generic@prompt -vvv wise.vote/wise.vote.yml \
     $@
--- scripts/deploy-staging-wise.vote.sh
+++ scripts/deploy-staging-wise.vote.sh
@@ -3,7 +3,7 @@ set -e # fail on first error
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.." # parent dir
 cd "${DIR}"
 
-ansible-playbook -i wise.vote/staging.yml \
+ANSIBLE_CONFIG="${DIR}/../ansible.cfg" ansible-playbook -i wise.vote/staging.yml \
     --extra-vars "@~/p/ansible/staging.yml" \
     --vault-id jblew_staging@prompt \
     --vault-id generic@prompt -vv wise.vote/wise.vote.yml \
