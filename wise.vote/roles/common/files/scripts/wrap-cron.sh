4fc1f6360c9240b9390bcee57df7a7da244e0728
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-01-29 16:50:12 +0100

    fix: fix cron job logs

--- wise.vote/roles/common/files/scripts/wrap-cron.sh
+++ wise.vote/roles/common/files/scripts/wrap-cron.sh
@@ -6,13 +6,13 @@ TIME_START=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 echo ""
 echo "" >> "${CRON_LOG_FILE}"
-echo "Cron job start ${JOB_NAME} at ${TIME_START}"
-echo "Cron job start ${JOB_NAME} at ${TIME_START}" >> "${CRON_LOG_FILE}"
+echo "[${TIME_START}] start ${CRON_JOB_NAME}"
+echo "[${TIME_START}] start ${CRON_JOB_NAME}" >> "${CRON_LOG_FILE}"
 
-eval "$@" 2>&1 >> "${CRON_LOG_FILE}"
+eval "$@" >> "${CRON_LOG_FILE}" 2>&1
 
 TIME_FINISH=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-echo "Cron job finish ${JOB_NAME} at ${TIME_FINISH}"
-echo "Cron job finish ${JOB_NAME} at ${TIME_FINISH}" >> "${CRON_LOG_FILE}"
+echo "[${TIME_FINISH}] finish ${CRON_JOB_NAME}"
+echo "[${TIME_FINISH}] finish ${CRON_JOB_NAME}" >> "${CRON_LOG_FILE}"
 
 echo "Done"
